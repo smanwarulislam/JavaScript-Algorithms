adf7ab3b26cbfa5451b25f562186f1c212122189
S.M. Anwarul Islam <engg.anwarbd@gmail.com>
2018-12-02 19:30:53 +0600

    Variables Added

---
+++ Section 10 (Review - Variables)/variable_lab.js
@@ -0,0 +1,22 @@
+// review
+// variables are containers that store data
+// must begin with upper/lower case a - z, $, _
+// camelCase best practice
+
+// practice
+var example = 'first example';
+
+// challenge
+var 1stPet = 'Bear';
+var BaseballTeam = 'Dodgers';
+var favoritedayoftheweek = 'Friday';
+var favorite color = 'green';
+var var = 'car';
+
+// possible solutions
+var pet1 = 'Bear';
+var baseballTeam = 'Dodgers';
+var favoriteDayOfTheWeek = 'Friday';
+var favorite_color = 'green';
+var favoriteColor = 'green';
+var travel = 'car';
---
+++ Section 10 (Review - Variables)/variable_whiteboard.js
@@ -0,0 +1,26 @@
+var transportation = 'car';
+
+// variable name (invalid)
+var 1stPet = 'Bear';    // starts with number
+var var = 'variable';   // keyword/reserved word
+var first name = 'John';    // space
+
+// variable name (valid)
+var fruit = 'apple';    // starts with upper/lower case letter
+var $money = 5; // starts with $
+var _weather = 'hot';   // begins with underscore
+
+// best practice
+// camelCase
+lunchcostperstudent = 5;    // bad practice
+LUNCHCOSTPERSTUDENT = 5;    // bad practice
+lunchCostPerStudent = 5;    // best practice
+
+// human readable
+var x = 25;
+var applesFromTree = 25;
+
+// JavaScript as Unscripted Dynamic Language
+var transportation = 'car'; | typeof transportation;    // string
+var passLieDetector = false;    | typeof passLieDetector;   // boolean
+var groceryBill = 74;   | typeof groceryBill;   // number
